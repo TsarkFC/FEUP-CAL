5456befd0aae1327f0c42e071e1247be04231ece
TsarkFC <digasromao2000@gmail.com>
2020-03-06 16:41:35 +0000

    Started Greedy algorithms worksheet

--- cal_fp01_CLion/Tests/Partitioning.cpp
+++ cal_fp01_CLion/Tests/Partitioning.cpp
@@ -3,7 +3,10 @@
  */
 
 #include "Partitioning.h"
+#include <vector>
+#include <iostream>
 
+using namespace std;
 
 int s_recursive(int n,int k)
 {
@@ -13,7 +16,22 @@ int s_recursive(int n,int k)
 
 int s_dynamic(int n,int k)
 {
-    
+    int c[n][n];
+
+    for (int i = 0; i < n; i++){
+        c[0][i] = 1;
+        c[i][i] = 1;
+
+    }
+
+    for (int i = 1; i < k; i++){
+        for (int j = 0; j < (n-k); j++){
+            cout << c[i][j+i] << " : " << c[i-1][j+i] << "   ->   " << (i+1)*c[i][j+i] + c[i-1][j+i] << endl;
+            c[i][j+i+1] = (i+1)*c[i][j+i] + c[i-1][j+i];
+        }
+    }
+
+    return c[k-1][n-1];
 }
 
 
@@ -30,7 +48,13 @@ int b_recursive(int n)
 
 int b_dynamic(int n)
 {
-	return 0;
+    int sum = 0;
+
+    for (int i = 1; i < n + 1; i++){
+        sum += s_dynamic(n, i);
+    }
+
+    return sum;
 }
 
 
